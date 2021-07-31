38a34b520b0cc955f8f364a976c9e144556d1f3d
Sean Heinen <seanheinen88@gmail.com>
2021-07-31 17:49:51 +0300

    Angular pipes

--- src/feature/user/user-details/user-details.ts
+++ src/feature/user/user-details/user-details.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-user-details',
-    templateUrl: 'user-details.html',
-    styleUrls: ['user-details.scss']
+  selector: 'app-user-details',
+  templateUrl: 'user-details.html',
+  styleUrls: ['user-details.scss']
 })
 export class UserDetailsComponent {
+
+  // display as percentage with 2 integers before and after decimal point (fill with zeros)
+  public readonly vat = .1899 ;
+
+  // integer before decimal point must be 4 digits (fill with zeros)
+  // integer after decimal point must be 2 digits (fill with zeros)
+  // if integer after decimal point is more than 3 digits round to 3 digits
+  public readonly price = 20.1325678;
+
+  // display in format dd/mm/yyyy hh:mm with locale UTC+3
+  public readonly time = 1627741984178;
 
 }
